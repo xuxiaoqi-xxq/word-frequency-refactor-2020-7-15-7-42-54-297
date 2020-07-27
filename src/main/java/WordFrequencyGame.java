e6f3576a0669e3937b8cc457e1fd4150dc544976
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-27 15:44:46 +0800

    [eva] refactor: delete useless comments

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -20,7 +20,6 @@ public class WordFrequencyGame {
 
             try {
 
-                //split the input string with 1 to n pieces of spaces
                 String[] arr = inputStr.split(SPACE_REGEX);
 
                 List<Input> inputList = new ArrayList<>();
@@ -29,7 +28,6 @@ public class WordFrequencyGame {
                     inputList.add(input);
                 }
 
-                //get the map for the next step of sizing the same word
                 Map<String, List<Input>> map =getListMap(inputList);
 
                 List<Input> list = new ArrayList<>();
@@ -56,7 +54,6 @@ public class WordFrequencyGame {
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> map = new HashMap<>();
         for (Input input : inputList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
             if (!map.containsKey(input.getValue())) {
                 ArrayList arr = new ArrayList<>();
                 arr.add(input);
